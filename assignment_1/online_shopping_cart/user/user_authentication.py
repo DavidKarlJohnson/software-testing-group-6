779bd391c603b608d51d4bfde731918566141c50
David Johnson <david_karl_johnson@hotmail.com>
2025-11-14 17:03:19 +0100

    Complete task 3.1

---
+++ assignment_1/online_shopping_cart/user/user_authentication.py
@@ -0,0 +1,70 @@
+###############################
+# USER AUTHENTICATION CLASSES #
+###############################
+
+from rich.console import Console
+
+from assignment_1.online_shopping_cart.user.user_data import UserDataManager
+
+
+class PasswordValidator:
+    @staticmethod
+    def __has_capital_letter(string: str) -> bool:
+        for char in string:
+            if char.isupper():
+                return True
+        return False
+
+    @staticmethod
+    def __has_special_symbol(string: str) -> bool:
+        for char in string:
+            if (not char.isdigit() and
+                    not char.isalpha() and
+                    not char == ' '):
+                return True
+        return False
+
+    @staticmethod
+    def __is_minimum_length(string: str, min_len: int) -> bool:
+        if len(string) >= min_len:
+            return True
+        return False
+
+    @staticmethod
+    def is_valid(password) -> bool:
+        if (PasswordValidator.__has_capital_letter(password) and
+            PasswordValidator.__has_special_symbol(password) and
+            PasswordValidator.__is_minimum_length(password, 8)):
+                return True
+        return False
+
+
+class UserAuthenticator:
+
+    @staticmethod
+    def login(username, password, data) -> dict[str, str | float] | None:
+        is_user_registered: bool = False
+
+        for entry in data:
+            if entry['username'].lower() == username.lower():
+                is_user_registered = True
+            if is_user_registered:
+                if entry['password'].lower() == password.lower():
+                    print('Successfully logged in.')
+                    return {
+                        'username': entry['username'],
+                        'wallet': entry['wallet']
+                    }
+                break
+
+        if not is_user_registered:
+            print('User is not registered.')
+        else:
+            print('Login failed.')
+        return None
+
+    @staticmethod
+    def register(username, password, data) -> None:
+        old_users = UserDataManager.load_users()
+        old_users.append({'username': username, 'password': password, 'wallet': data})
+        UserDataManager.save_users(old_users)
---
+++ assignment_1/online_shopping_cart/user/user_login.py
@@ -0,0 +1,50 @@
+from online_shopping_cart.user.user_authentication import UserAuthenticator
+from online_shopping_cart.user.user_interface import UserInterface
+from online_shopping_cart.user.user_data import UserDataManager
+from samba.dcerpc.samr import Password
+
+from assignment_1.online_shopping_cart.user.user_authentication import PasswordValidator
+
+
+########################
+# USER LOGIN FUNCTIONS #
+########################
+
+def is_quit(input_argument: str) -> bool:
+    return input_argument.lower() == 'q'
+
+
+
+def login() -> dict[str, str | float] | None:
+    username: str = UserInterface.get_user_input(prompt="Enter your username (or 'q' to quit): ")
+    if is_quit(input_argument=username):
+        exit(0)  # The user has quit
+
+    password: str = UserInterface.get_user_input(prompt="Enter your password (or 'q' to quit): ")
+    if is_quit(input_argument=password):
+        exit(0)   # The user has quit
+
+    is_authentic_user: dict[str, str | float] = UserAuthenticator().login(
+        username=username,
+        password=password,
+        data=UserDataManager.load_users()
+    )
+    if is_authentic_user is not None:
+        return is_authentic_user
+
+    # TODO: Task 1: prompt user to register when not found
+    register_new_user: str = UserInterface.get_user_input(prompt=f"Would you like to register new user  '{username}': y/n ? ")
+    if register_new_user.lower() != 'y':
+        return None
+
+    new_password: str = UserInterface.get_user_input(prompt="Password: ")
+    if PasswordValidator.is_valid(new_password):
+        UserAuthenticator.register(username=username, password=new_password, data=0.0)
+        print('Saved new user')
+    else:
+        print('Invalid password, please give a password with a minimum length of 8 characters, at lease one capital letter and one special symbol')
+
+
+
+
+    return None
