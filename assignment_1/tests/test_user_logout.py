0f4626e227a7de697bf9c10db018ef32dedc640d
David Johnson <david_karl_johnson@hotmail.com>
2025-11-22 09:50:13 +0100

    Refactor 'user_logout' tests

--- assignment_1/tests/test_user_logout.py
+++ assignment_1/tests/test_user_logout.py
@@ -1,17 +1,14 @@
 import pytest
-import os
-
 from assignment_1.online_shopping_cart.checkout.shopping_cart import ShoppingCart
 from assignment_1.online_shopping_cart.product.product import Product
 from assignment_1.online_shopping_cart.user.user_logout import logout
-
 
 
 # ----- TASK 3.1 -----
 # TODO: Write 10 test cases for the function 'logout' located in  /online_shopping_cart/user/user_logout.py
 
 
-# Test #1: Test different variants of 'yes' for confirming logout
+# Test #1-5: Test different variants of 'yes' for confirming logout
 @pytest.mark.parametrize('user_input, expected',
                          [('y', True),
                           ('yes', True),
@@ -20,33 +17,26 @@ from assignment_1.online_shopping_cart.user.user_logout import logout
                           ('no', False)])
 def test_logout_confirmation_yes(mocker, user_input, expected):
     shopping_cart = ShoppingCart()
-    mocker.patch(
-        "assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input",
-        side_effect=[user_input])
-
-    result = logout(shopping_cart)
-    assert result == expected
+    mocker.patch("assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input", side_effect=[user_input])
+    assert logout(shopping_cart) == expected
 
 
-# Test #2: Test different variants of 'no' for canceling logout
+# Test #6-12: Test different variants of 'no' for canceling logout
 @pytest.mark.parametrize('user_input, expected',
                          [('n', False),
                           ('no', False),
                           ('abc', False),
                           ('!', False),
                           (' ', False),
+                          (' y', False),
                           ('y', True)])
 def test_logout_confirmation_no(mocker, user_input, expected):
     shopping_cart = ShoppingCart()
-    mocker.patch(
-        "assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input",
-        side_effect=[user_input])
-
-    result = logout(shopping_cart)
-    assert result == expected
+    mocker.patch("assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input", side_effect=[user_input])
+    assert logout(shopping_cart) == expected
 
 
-# Test #3: Test capitalization of confirmation to logout
+# Test #13-16: Test capitalization of confirmation to logout
 @pytest.mark.parametrize('user_input, expected',
                          [('Y', True),
                           ('Yes', True),
@@ -54,57 +44,16 @@ def test_logout_confirmation_no(mocker, user_input, expected):
                           ('No', False)])
 def test_logout_confirmation_capitalization(mocker, user_input, expected):
     shopping_cart = ShoppingCart()
-    mocker.patch(
-        "assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input",
-        side_effect=[user_input])
-
-    result = logout(shopping_cart)
-    assert result == expected
+    mocker.patch("assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input", side_effect=[user_input])
+    assert logout(shopping_cart) == expected
 
 
-# Test #4: Non-empty cart warning when logging out
+# Test #17: Non-empty cart warning when logging out
 def test_logout_with_non_empty_cart(mocker, capsys):
     shopping_cart = ShoppingCart()
     shopping_cart.add_item(Product(name='Carrot', price=10.0, units=1))
     shopping_cart.add_item(Product(name='Egg', price=20.0, units=2))
     shopping_cart.add_item(Product(name='Shoe', price=30.0, units=2))
-
-    mocker.patch(
-        "assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input",
-        side_effect='n')
-
+    mocker.patch("assignment_1.online_shopping_cart.user.user_logout.UserInterface.get_user_input", side_effect='n')
     logout(shopping_cart)
-    captured_output = capsys.readouterr().out
-    assert captured_output == 'Your cart is not empty. You have the following items:\n''Carrot - $10.0 - Units: 1\n''Egg - $20.0 - Units: 2\n''Shoe - $30.0 - Units: 2\n'
-
-
-def test_logout5():
-    # NOTE: Rename function to something appropriate
-    pass
-
-
-def test_logout6():
-    # NOTE: Rename function to something appropriate
-    pass
-
-
-def test_logout7():
-    # NOTE: Rename function to something appropriate
-    pass
-
-
-def test_logout8():
-    # NOTE: Rename function to something appropriate
-    pass
-
-
-def test_logout9():
-    # NOTE: Rename function to something appropriate
-    pass
-
-
-def test_logout10():
-    # NOTE: Rename function to something appropriate
-    pass
-
-
+    assert capsys.readouterr().out == 'Your cart is not empty. You have the following items:\n''Carrot - $10.0 - Units: 1\n''Egg - $20.0 - Units: 2\n''Shoe - $30.0 - Units: 2\n'
