13bc4473c7d79fe80ba3322f2d4df54eccd37a1c
David Johnson <david_karl_johnson@hotmail.com>
2025-12-03 17:44:47 +0100

    Implement true evaluation for predicate test of Assignment 2: Task 3

--- assignment_2/tasks_3_and_4/tests/test_predicate_coverage.py
+++ assignment_2/tasks_3_and_4/tests/test_predicate_coverage.py
@@ -5,3 +5,17 @@ from tasks_3_and_4.air_traffic_control import air_traffic_control
 # Predicate coverage. (3 points)
 # Create a test suite that achieves predicate coverage. The set of predicates to be
 # considered are those in the if conditions in the decision making process.
+
+
+# Test 1: Make the predicate in the if-condition of the
+#         function 'air_traffic_control' TRUE
+def test_predicate_true():
+    assert air_traffic_control(
+        True,
+       True,
+       100,
+       False,
+       30,
+       1001,
+       4,
+       False) == 'Landing Allowed'
