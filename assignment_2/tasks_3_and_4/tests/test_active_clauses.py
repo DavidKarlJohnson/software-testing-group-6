f2c5cb62013fc58b522c95ca45417c5d3c42d9ea
David Johnson <david_karl_johnson@hotmail.com>
2025-12-15 13:30:30 +0100

    Create preliminary solution for assignment 2, task 4a (active clauses)

--- assignment_2/tasks_3_and_4/tests/test_active_clauses.py
+++ assignment_2/tasks_3_and_4/tests/test_active_clauses.py
@@ -7,3 +7,25 @@ from tasks_3_and_4.air_traffic_control import air_traffic_control
 # particular predicate such that if the truth value of c is changed while retaining
 # the truth value of the other clauses, then the truth value of the predicate also
 # changes.
+
+"""
+In the if-statement of the function we've got five clauses separated by AND. Imagine a situation where these clauses
+are set in such a way that the if-branch is taken:
+
+* runway_available = True
+* safe_speed = True
+* emergency = False
+* safe_weather = True
+* acceptable_traffic = True
+
+Changing the variable 'plane_speed', so that the derived condition 'safe_speed' is changed to False will make the entire
+predicate (the if-statement) False. Therefore, making it an active clause. 
+Same thing with the 'emergency' variable (atomic), from False to True.
+And also the variable 'airport_traffic', setting the derived condition 'acceptable_traffic' from True to False. 
+
+SOLUTION:
+We've identified the following three active clauses (note: atomic):
+1) plane_speed
+2) emergency
+3) airport_traffic
+"""
